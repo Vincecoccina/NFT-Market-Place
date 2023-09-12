eb6e2022d1857d1ec286644661b7a21aed439919
Vince <v.coccina@orange.fr>
2023-09-12 11:18:47 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,11 @@ import Button from "./UIElement/Button";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logo.png";
-
+import cross from "../assets/cross.png";
+import menu from "../assets/menu.png";
 import classes from "../styles/Navbar.module.css";
 
-const MenuItems = ({ active, setActive }) => {
+const MenuItems = ({ active, setActive, setMobileIsOpen }) => {
   const generateLink = (i) => {
     switch (i) {
       case 0:
@@ -16,7 +17,7 @@ const MenuItems = ({ active, setActive }) => {
       case 1:
         return "/created-nfts";
       case 2:
-        return "/my-nfts"     
+        return "/my-nfts";
       default:
         return "/";
     }
@@ -26,7 +27,13 @@ const MenuItems = ({ active, setActive }) => {
     <ul className={classes.ItemGroup_Navbar}>
       {["Explore NFTs", "Listed NFTs", "My NFTs"].map((item, i) => {
         return (
-          <li key={i} onClick={() => {setActive(item)}}>
+          <li
+            key={i}
+            onClick={() => {
+              setActive(item);
+              setMobileIsOpen(false);
+            }}
+          >
             <Link
               href={generateLink(i)}
               className={
@@ -44,13 +51,22 @@ const MenuItems = ({ active, setActive }) => {
 
 const Navbar = () => {
   const [active, setActive] = useState("Explore NFTs");
+  const [hasConnected, setHasConnected] = useState(false);
+  const [mobileIsOpen, setMobileIsOpen] = useState(false);
+  const router = useRouter();
 
   return (
     <nav className={classes.containerNavbar}>
       <div className={classes.containerItem_Navbar}>
         <Link href="/">
           <div className={classes.containerLogo_Navbar}>
-            <Image src={logo} width={40} height={40} objectFit="contain" />
+            <Image
+              src={logo}
+              width={40}
+              height={40}
+              objectFit="contain"
+              alt="logo"
+            />
             <p>NFT Market Place</p>
           </div>
         </Link>
@@ -59,10 +75,62 @@ const Navbar = () => {
       <div className={classes.containerMenuItem_Navbar}>
         <MenuItems active={active} setActive={setActive} />
         <div className={classes.containerBtn_Navbar}>
-          <Button>Create NFT</Button>
-          <Button invert>Connect</Button>
+          <Button
+            onClick={() => {
+              if (hasConnected) {
+                setActive("");
+                router.push("/create-nft");
+              }
+              // Ajouter le else pour la connection metamask
+            }}
+          >
+            {hasConnected ? "Create" : "Connect"}
+          </Button>
         </div>
       </div>
+
+      {/* MOBILE NAV */}
+      <div className={classes.containerMenuItem_NavbarMobile}>
+        {mobileIsOpen ? (
+          <Image
+            src={cross}
+            height={30}
+            width={30}
+            alt="close"
+            onClick={() => setMobileIsOpen(false)}
+          />
+        ) : (
+          <Image
+            src={menu}
+            height={35}
+            width={35}
+            alt="menu"
+            onClick={() => setMobileIsOpen(true)}
+          />
+        )}
+      </div>
+      {mobileIsOpen && (
+        <div className={classes.modalItem_NavbarMobile}>
+          <MenuItems
+            active={active}
+            setActive={setActive}
+            setMobileIsOpen={setMobileIsOpen}
+          />
+          <div className={classes.containerBtn_Navbar}>
+            <Button
+              onClick={() => {
+                if (hasConnected) {
+                  setActive("");
+                  router.push("/create-nft");
+                }
+                // Ajouter le else pour la connection metamask
+              }}
+            >
+              {hasConnected ? "Create" : "Connect"}
+            </Button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
--- src/components/UIElement/Button.jsx
+++ src/components/UIElement/Button.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import classes from "../../styles/Button.module.css"
+import React from "react";
+import classes from "../../styles/Button.module.css";
 
 const Button = (props) => {
   return (
-    <button type='button' className={props.invert ? classes.invertBtn : classes.classicBtn}>{props.children}</button>
-  )
-}
+    <button
+      type="button"
+      className={props.invert ? classes.invertBtn : classes.classicBtn}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
